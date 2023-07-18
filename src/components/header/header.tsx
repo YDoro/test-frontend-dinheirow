2cffd82a2e004da879c9ec2e6b4e8f95a35b9557
Yan Mello <yan_mvd@hotmail.com>
2023-07-18 16:13:09 -0300

    fix: using the primary red color on logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,26 @@
-"use client"
-import Link from "next/link"
-import { useState } from "react"
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
-const Header = () =>{
- const [logoText,setLogoText] =  useState("MARVEL")
- return (
-    <div  data-testid="header" className="bg-primaryGray flex flex-row justify-center border-b-solid border-b border-b-red-700" 
-         onMouseEnter={()=>setLogoText("MARVEL")}
-         onMouseLeave={()=>setLogoText("M")}>
-        <Link key={logoText} className="animate-fadeIn text-white bg-red-700  p-1 text-6xl font-marvel" href="/" title="home">{logoText}</Link>
+const Header = () => {
+  const [logoText, setLogoText] = useState("MARVEL");
+  return (
+    <div
+      data-testid="header"
+      className="bg-primaryGray flex flex-row justify-center border-b-solid border-b border-b-primaryRed"
+      onMouseEnter={() => setLogoText("MARVEL")}
+      onMouseLeave={() => setLogoText("M")}
+    >
+      <Link
+        key={logoText}
+        className="animate-fadeIn text-white bg-primaryRed  p-1 text-6xl font-marvel"
+        href="/"
+        title="home"
+      >
+        {logoText}
+      </Link>
     </div>
-    )
+  );
+};
 
-}
-
-export default Header
+export default Header;
