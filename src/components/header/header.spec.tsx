c7bb46b24086ce87a131ce0d4c2615ac38a59dd0
Yan Mello <yan_mvd@hotmail.com>
2023-07-18 13:55:25 -0300

    test: ensure header will display M on mouse leave

--- src/components/header/header.spec.tsx
+++ src/components/header/header.spec.tsx
@@ -4,15 +4,18 @@ import Header from "./header";
 describe("<Header/>", () => {
   test("should render with MARVEL on 'LOGO'", () => {
     render(<Header />);
-    expect(screen.getByTitle("home").textContent).toEqual("MARVEL")
+    expect(screen.getByTitle("home").textContent).toEqual("MARVEL");
   });
 
-  test("should render with M on 'LOGO'", async() => {
+  test("should render M on mouse Leave and MARVEL on mouse enter", async () => {
     render(<Header />);
-    fireEvent.mouseLeave(screen.getByTestId("header"))
+    fireEvent.mouseLeave(screen.getByTestId("header"));
     await waitFor(() => {
       expect(screen.getByTitle("home").textContent).toEqual("M");
     });
-  
+    fireEvent.mouseEnter(screen.getByTestId("header"));
+    await waitFor(() => {
+      expect(screen.getByTitle("home").textContent).toEqual("MARVEL");
+    });
   });
 });
