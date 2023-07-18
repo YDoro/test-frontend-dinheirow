56e6c21d22d37efa32eb2eab5ccee51a12989473
Yan Mello <yan_mvd@hotmail.com>
2023-07-18 13:51:06 -0300

    feat: add component testId

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 const Header = () =>{
  const [logoText,setLogoText] =  useState("MARVEL")
  return (
-    <div className="bg-primaryGray flex flex-row justify-center border-b-solid border-b border-b-red-700" 
+    <div  data-testid="header" className="bg-primaryGray flex flex-row justify-center border-b-solid border-b border-b-red-700" 
          onMouseEnter={()=>setLogoText("MARVEL")}
          onMouseLeave={()=>setLogoText("M")}>
         <Link key={logoText} className="animate-fadeIn text-white bg-red-700  p-1 text-6xl font-marvel" href="/" title="home">{logoText}</Link>
