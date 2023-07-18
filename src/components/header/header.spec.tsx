243c0b35be13ee4d0dbf2d05a3c78ed0f4b028c3
Yan Mello <yan_mvd@hotmail.com>
2023-07-18 13:51:43 -0300

    test: ensure header will display M instead Marvel on mouse leave

--- src/components/header/header.spec.tsx
+++ src/components/header/header.spec.tsx
@@ -1,9 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Header from "./header";
 
 describe("<Header/>", () => {
   test("should render with MARVEL on 'LOGO'", () => {
     render(<Header />);
     expect(screen.getByTitle("home").textContent).toEqual("MARVEL")
+  });
+
+  test("should render with M on 'LOGO'", async() => {
+    render(<Header />);
+    fireEvent.mouseLeave(screen.getByTestId("header"))
+    await waitFor(() => {
+      expect(screen.getByTitle("home").textContent).toEqual("M");
+    });
+  
   });
 });
