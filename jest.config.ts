7c999916dee9ff2adf4e7c1fb0ea30665b3d9752
Yan Mello <yan_mvd@hotmail.com>
2023-07-18 14:22:52 -0300

    chore: use swc instead babel

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,22 @@ import type { Config } from "jest";
 
 const config: Config = {
   testEnvironment: "jsdom",
+  rootDir: process.cwd(),
+  moduleFileExtensions: ["js", "ts", "tsx"],
+  transform: {
+    ".*\\.(tsx?)$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
