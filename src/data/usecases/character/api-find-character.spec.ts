b4d580c3982ce7e936445290103fa42a2b3082b3
Yan Mello <yan_mvd@hotmail.com>
2023-07-19 10:26:01 -0300

    test: ensure APIFindCharacter will call FindCharacterService with the given query

---
+++ src/data/usecases/character/api-find-character.spec.ts
@@ -0,0 +1,30 @@
+import { FindCharacterService } from "@/data/protocols/services/find-character-service";
+import { APIFindCharacter } from "./api-find-character";
+
+interface SUTTypes {
+  sut: APIFindCharacter;
+  fcs: FindCharacterService;
+}
+
+const makeSUT = (): SUTTypes => {
+  const fcs = makeFindCharacterService();
+  const sut = new APIFindCharacter(fcs);
+  return { sut, fcs };
+};
+
+const makeFindCharacterService = (): FindCharacterService => {
+  class FindCharacterServiceStub implements FindCharacterService {
+    async find(query: any): Promise<any> {
+      return await new Promise((resolve) => resolve([]));
+    }
+  }
+  return new FindCharacterServiceStub();
+};
+describe("find character", () => {
+  test("should call find character service with the query", async () => {
+    const { fcs, sut } = makeSUT();
+    const fcsSpy = jest.spyOn(fcs, "find");
+    await sut.find({ limit: 20, offset: 10 });
+    expect(fcsSpy).toHaveBeenCalledWith({ limit: 20, offset: 10 });
+  });
+});
