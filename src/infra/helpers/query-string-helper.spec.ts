346f68490886bb9a45ddb581f82aa879465bd7ea
Yan Mello <yan_mvd@hotmail.com>
2023-07-19 10:47:59 -0300

    test: ensure query string helper will return the expected value

--- src/infra/helpers/query-string-helper.spec.ts
+++ src/infra/helpers/query-string-helper.spec.ts
@@ -5,4 +5,13 @@ describe("query string helper", () => {
     const sut = new QueryStringHelper();
     expect(sut.fromObject({})).toEqual("");
   });
+  test("should return the expected query", () => {
+    const sut = new QueryStringHelper();
+    expect(
+      sut.fromObject({
+        foo: "hi there",
+        bar: "100%",
+      })
+    ).toEqual("?foo=hi%20there&bar=100%25");
+  });
 });
