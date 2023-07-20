c47fe9ee675a73bd79329fd2e7e3169d44527b13
Yan Mello <yan_mvd@hotmail.com>
2023-07-20 19:49:15 -0300

    chore: add a env ts file to provide fallbacks when possible

---
+++ src/main/config/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+  marvelAPI: process.env.MARVEL_API || "",
+  marvelPrivateKey: process.env.MARVEL_PRIVATE_KEY || "",
+  marvelPublicKey: process.env.MARVEL_PUBLIC_KEY || "",
+};
