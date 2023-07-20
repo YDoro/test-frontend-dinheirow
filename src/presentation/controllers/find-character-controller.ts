a795ee0684e6d8dc6f67a4fd4f0af8e25af819d5
Yan Mello <yan_mvd@hotmail.com>
2023-07-20 19:48:15 -0300

    feat: return a basic result

--- src/presentation/controllers/find-character-controller.ts
+++ src/presentation/controllers/find-character-controller.ts
@@ -2,11 +2,10 @@ import { FindCharacter } from "@/domain/usecases/find-character";
 import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
-//TODO - create a controller to be called by the nextJS API
 export class FindCharacterController implements Controller {
   constructor(private readonly findCharacter: FindCharacter) {}
   async handle(req: HttpRequest): Promise<HttpResponse> {
     const res = await this.findCharacter.find(req.query);
-    return { statusCode: 200, body: {} };
+    return { statusCode: 200, body: res };
   }
 }
