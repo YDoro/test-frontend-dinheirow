b938d43c1ccfa78d7a3c5bac4cbde5373d6c1641
Yan Mello <yan_mvd@hotmail.com>
2023-07-20 19:50:06 -0300

    feat: using the env values

--- src/main/factories/services/marvel-api-service-factory.ts
+++ src/main/factories/services/marvel-api-service-factory.ts
@@ -2,14 +2,15 @@ import { QueryStringHelper } from "@/infra/helpers/query-string-helper";
 import { MarvelAPIService } from "@/infra/services/marvel/marvel-api-service";
 import { CryptoAdapter } from "@/main/adapters/crypto-adapter";
 import { axiosAdapter } from "@/main/adapters/http-client";
+import { env } from "@/main/config/env";
 
 export const makeMarvelAPIService = () => {
   const hasher = new CryptoAdapter("md5");
-  const httpClient = axiosAdapter("get it from env");
+  const httpClient = axiosAdapter(env.marvelAPI);
   const qsHelper = new QueryStringHelper();
   const ts = new Date().getTime().toString();
-  const privateKey = "get it from env";
-  const publicKey = "get it from env";
+  const privateKey = env.marvelPrivateKey;
+  const publicKey = env.marvelPublicKey;
 
   return new MarvelAPIService(
     hasher,
