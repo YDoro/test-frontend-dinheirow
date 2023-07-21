b00acdc543d7e1d69dd25d19494619173c122a26
Yan Mello <yan_mvd@hotmail.com>
2023-07-21 10:01:19 -0300

    feat: returning parser character array instead service response

--- src/data/usecases/character/api-find-character.ts
+++ src/data/usecases/character/api-find-character.ts
@@ -1,10 +1,15 @@
+import { CharacterParser } from "@/data/protocols/helpers/character-parser";
 import { FindCharacterService } from "@/data/protocols/services/find-character-service";
 import { Character } from "@/domain/models/character";
 import { FindCharacter } from "@/domain/usecases/find-character";
 
 export class APIFindCharacter implements FindCharacter {
-  constructor(private readonly findCharacterService: FindCharacterService) {}
+  constructor(
+    private readonly findCharacterService: FindCharacterService,
+    private readonly characterParser: CharacterParser
+  ) {}
   async find(query: any): Promise<Character[]> {
-    return await this.findCharacterService.find(query);
+    const serviceResponse = await this.findCharacterService.find(query);
+    return this.characterParser.toCharacterArray(serviceResponse);
   }
 }
