9c8c139b79054ed71d24e9419f5d6cb11aabfb69
Yan Mello <yan_mvd@hotmail.com>
2023-07-21 10:01:52 -0300

    test: ensure api find character will call the character parser

--- src/data/usecases/character/api-find-character.spec.ts
+++ src/data/usecases/character/api-find-character.spec.ts
@@ -1,15 +1,30 @@
+import { CharacterParser } from "@/data/protocols/helpers/character-parser";
 import { FindCharacterService } from "@/data/protocols/services/find-character-service";
+import { Character } from "@/domain/models/character";
 import { APIFindCharacter } from "./api-find-character";
 
-interface SUTTypes {
-  sut: APIFindCharacter;
-  fcs: FindCharacterService;
-}
-
-const makeSUT = (): SUTTypes => {
+const makeSUT = () => {
   const fcs = makeFindCharacterService();
-  const sut = new APIFindCharacter(fcs);
-  return { sut, fcs };
+  const cp = makeCharacterParser();
+  const sut = new APIFindCharacter(fcs, cp);
+  return { sut, cp, fcs };
+};
+
+const fakeChar: Character = {
+  id: 1,
+  name: "mock boy",
+  comics: [{ name: "the adventures of the mock boy", uri: "" }],
+  description: "a strange hero that comes to test everyones patience",
+  thumbnail: { extension: "JPEG", path: "path/to/exhaust" },
+};
+
+const makeCharacterParser = () => {
+  class CharacterParserStub implements CharacterParser {
+    toCharacterArray(obj: any): Character[] {
+      return [fakeChar];
+    }
+  }
+  return new CharacterParserStub();
 };
 
 const makeFindCharacterService = (): FindCharacterService => {
@@ -31,6 +46,24 @@ describe("find character", () => {
   test("should throw if find character service throws", async () => {
     const { fcs, sut } = makeSUT();
     jest.spyOn(fcs, "find").mockRejectedValueOnce(new Error("mock"));
+    const promise = sut.find({ limit: 20, offset: 10 });
+    expect(promise).rejects.toThrow();
+  });
+
+  test("should call character parser the service output", async () => {
+    const { fcs, sut, cp } = makeSUT();
+    const fakeServiceReponse = { lorem: "ipsum", sit: "dollor" };
+    const cpSpy = jest.spyOn(cp, "toCharacterArray");
+    jest.spyOn(fcs, "find").mockResolvedValueOnce(fakeServiceReponse);
+    await sut.find({ limit: 20, offset: 10 });
+    expect(cpSpy).toHaveBeenCalledWith(fakeServiceReponse);
+  });
+
+  test("should throw if character parser throws", async () => {
+    const { sut, cp } = makeSUT();
+    jest.spyOn(cp, "toCharacterArray").mockImplementationOnce((_) => {
+      throw new Error("mocked");
+    });
     const promise = sut.find({ limit: 20, offset: 10 });
     expect(promise).rejects.toThrow();
   });
