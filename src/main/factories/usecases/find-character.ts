fcfd88f7e356b42314292289b01a85acb38d5d4d
Yan Mello <yan_mvd@hotmail.com>
2023-07-24 14:25:26 -0300

    fix: update factory to pass a parser to the constructor

--- src/main/factories/usecases/find-character.ts
+++ src/main/factories/usecases/find-character.ts
@@ -1,6 +1,10 @@
 import { APIFindCharacter } from "@/data/usecases/character/api-find-character";
+import { MarvelCharacterParser } from "@/infra/services/marvel/marvel-character-parser";
 import { makeMarvelAPIService } from "../services/marvel-api-service-factory";
 
 export const makeMarvelFindCharacterUC = () => {
-  return new APIFindCharacter(makeMarvelAPIService());
+  return new APIFindCharacter(
+    makeMarvelAPIService(),
+    new MarvelCharacterParser()
+  );
 };
