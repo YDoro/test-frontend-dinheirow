068078a6e69e343d545261bdc38638e37e4c7e43
Yan Mello <yan_mvd@hotmail.com>
2023-08-01 11:25:04 -0300

    feat: just a initial structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+"use client";
+import CharacterCard from "@/components/character-card/character-card";
+import { Character } from "@/domain/models/character";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [chars, setChars] = useState<Character[]>([]);
+  useEffect(() => {
+    fetch("/api/character").then((r) => {
+      r.json().then((data) => {
+        setChars(data);
+      });
+    });
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Worlds!
+    <main className="flex min-h-screen p-24 ">
+      <div className="flex flex-wrap max-w-[1040px] ml-auto mr-auto ">
+        {chars.map((char) => (
+          <div className="p-2">
+            <CharacterCard
+              image={`${char.thumbnail.path}.${char.thumbnail.extension}`}
+              name={char.name}
+              key={char.id}
+              nameComplement=""
+            />
+          </div>
+        ))}
+      </div>
     </main>
-  )
+  );
 }
